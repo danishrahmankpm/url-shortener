4aec891ebe6a5a784388fe20b21803400cc8de51
danishrahmankpm <140050542+danishrahmankpm@users.noreply.github.com>
2025-07-25 11:49:45 +0530

    setup

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## Overview
 A simple URL shortening service similar to bit.ly or tinyurl. 
+
+## Notes
+Run the run_tests.py file to run the tests
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -20,16 +20,16 @@ def test_shorten_invalid_url(client):
     assert response.get_json()["error"] == "Invalid URL"
 
 def test_redirect_and_click_tracking(client):
-    # Step 1: Shorten URL
+    ##Shorten URL
     post_resp = client.post('/api/shorten', json={"url": "https://example.com"})
     short_code = post_resp.get_json()["short_code"]
 
-    # Step 2: Redirect
+    ##Redirect
     redirect_resp = client.get(f"/{short_code}", follow_redirects=False)
     assert redirect_resp.status_code == 302
     assert redirect_resp.headers["Location"] == "https://example.com"
 
-    # Step 3: Stats
+    ##Stats
     stats_resp = client.get(f"/api/stats/{short_code}")
     stats = stats_resp.get_json()
     assert stats["url"] == "https://example.com"
